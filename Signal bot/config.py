cc78031efc07b7c3c015865535d2333eccb68cc5
REXwebc <167573160+REXwebc@users.noreply.github.com>
2025-03-01 15:02:01 +0700

    Add files via upload

---
+++ Signal bot/config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "8150173890:AAFY3HzR09ZV-1wle-fO73MheogolJEUk4Y"  # токен ботика
+CHANNEL_URL = "t.me/winsearch"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002449628382"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002286663786"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/tokentablesupport"
+ADMIN_ID = 6386410671 # Ваш айди без скобочек
+PROMO = "UDN23" # промокод
+ADMIN_ID1 = 967349759
+
+
